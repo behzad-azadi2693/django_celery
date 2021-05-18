b4f6c51b16d05d3eed2f8f08f1b34daba938ec86
joeliberal <joe.libral2693@gmail.com>
2021-05-18 18:15:46 +0430

    running

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'web',
+        'USER': 'admin',
+        'PASSWORD':'1234qwer',
+        'HOST':'postgres-container',
+        'PORT':5432
     }
 }
 
